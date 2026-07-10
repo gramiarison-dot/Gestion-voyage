087b43b2540276abb5867780de4354c1e3d470c4
gramiarison-dot <gramiarison@gmail.com>
2026-07-10 13:27:04 +0200

    Update stats.js

--- routes/stats.js
+++ routes/stats.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
-
+const { authenticate, isAdmin } = require('../Middleware/auth');
 // Route de statistiques (dashboard)
 router.get('/', (req, res) => {
     res.json({ message: 'Statistiques (à implémenter)' });
